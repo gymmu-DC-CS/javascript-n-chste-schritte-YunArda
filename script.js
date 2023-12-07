3b238857d12aab8b75141189959ac65c456fa319
federico69 <e220909@edu.sbl.ch.com>
2023-12-07 12:14:28 +0100

    Aufgabe 5&6

--- script.js
+++ script.js
@@ -4,7 +4,59 @@ export function aufgabe01(args) {
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    result.push(currentElement)
+    if (currentElement != "e") {
+      if (currentElement != "E") result.push(currentElement)
+    }
   }
   return result.join("")
+}
+
+export function aufgabe02(args) {
+  return args.toUpperCase()
+}
+
+export function aufgabe03(args) {
+  const input = args
+  let result = 0
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement === "e" || currentElement === "E") {
+      result++
+    }
+  }
+  return result
+}
+
+export function aufgabe04(args) {
+  const input = args
+  let result = 0
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement === " " && input[i - 1] != "-") {
+      result++
+    }
+  }
+  return result + 1
+}
+
+export function aufgabe05(args) {
+  const input = args
+  if (input.toLowerCase() === input) {
+    return false
+  } else {
+    return true
+  }
+}
+
+export function aufgabe06(args) {
+  const input = args
+  const result = []
+
+  return result.join("")
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+  }
 }
